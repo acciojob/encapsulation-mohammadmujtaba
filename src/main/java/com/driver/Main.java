8af9cc9f6f17e700cc3814899d31f61c9b5d582c
mohammadmujtaba <mohammadmujtaba262001@gmail.com>
2023-04-12 20:10:24 +0530

    Commit 1 | Assignment 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly ob = new RWOnly();
+
+        ob.setName("Mujtaba");
+        System.out.println(ob.getName());
+    }
+
 }
